104fc5a78a6caadcc3e81c83e8e01293969d844a
timoi <julien.maillard7@protonmail.com>
2021-06-13 16:50:40 +0200

    Implementation done, fully tested

--- test/test_aes.py
+++ test/test_aes.py
@@ -1,11 +1,10 @@
 import sys
 import numpy as np
-
 from Crypto.Cipher import AES as pyAES
-
 sys.path.insert(1, '..')
-
+from aes_reference import AES as refAES
 from aes import AES
+
 
 def test_encryption():
     key = np.array([
@@ -15,27 +14,31 @@ def test_encryption():
         [0x09, 0xcf, 0x4f, 0x3c]
         ], dtype=np.uint8)
 
+    ref_aes = refAES(int.from_bytes(key.tobytes(), 'big'))
+    my_aes = AES(key)
+    pts = np.random.randint(0, 256,
+            size=(10, 4, 4), dtype=np.uint8)
+    #pts = np.zeros((3, 4, 4), dtype=np.uint8)
+    ciphertexts = my_aes.encrypt(pts)
 
-    plaintexts = np.random.randint(0, 256,
-                                   size=(3, 4, 4),
-                                   dtype=np.uint8)
+    for i, pt in enumerate(pts):
+        good_cipher = ref_aes.encrypt(int.from_bytes(pt.tobytes(), 'big'))
+        assert int.from_bytes(ciphertexts[i].tobytes(), 'big') == good_cipher
 
-    cipher = pyAES.new(key.T.tobytes(), pyAES.MODE_ECB)
+def test_encryption2():
+    key = np.array([
+        [0x2b, 0x7e, 0x15, 0x16],
+        [0x28, 0xae, 0xd2, 0xa6],
+        [0xab, 0xf7, 0x15, 0x88],
+        [0x09, 0xcf, 0x4f, 0x3c]
+        ], dtype=np.uint8)
 
-    good_ciphers = []
-    for i in range(plaintexts.shape[0]):
-        ciphertext = cipher.encrypt(plaintexts[i].tobytes())
-        good_ciphers.append(np.frombuffer(ciphertext,
-                            dtype=np.uint8).reshape(4,4))
+    ref_aes = pyAES.new(key.tobytes(),pyAES.MODE_ECB)
+    my_aes = AES(key)
+    pts = np.random.randint(0, 256,
+            size=(100, 4, 4), dtype=np.uint8)
+    ciphertexts = my_aes.encrypt(pts)
 
-    good_ciphers = np.array(good_ciphers)
-
-    mycipher = AES(key)
-    computed_ciphers = mycipher.encrypt(plaintexts)
-
-    for i in range(good_ciphers.shape[0]):
-        print(good_ciphers[i])
-        print('-----vs ----')
-        print(computed_ciphers[i])
-        print('\n\n')
-    assert np.all(computed_ciphers == good_ciphers)
+    for i, pt in enumerate(pts):
+        good_cipher = ref_aes.encrypt(pt.tobytes())
+        assert ciphertexts[i].tobytes() == good_cipher
--- test/test_aes_utils.py
+++ test/test_aes_utils.py
@@ -9,73 +9,50 @@ from aes_utils import ShiftRows
 from aes_utils import MixColumns
 from aes_utils import AddRoundKey
 from aes_utils import ExpandKeys
+from aes_reference import AES as refAES
+from aes_reference import text2matrix
+
 
 def test_SubBytes():
+    pts = np.random.randint(0, 256,
+                           size=(200,4,4), dtype=np.uint8)
+    goods = []
+    for pt in pts:
+        int_pt = int.from_bytes(pt.tobytes(), 'big')
+        goods.append(refAES.sub_bytes(text2matrix(int_pt)))
+    goods = np.array(goods)
+    updated = SubBytes(pts)
 
-    states = np.random.randint(0, 256, 
-                               size=(10, 4, 4),
-                               dtype=np.uint8)
-    updated = SubBytes(states)
+    assert np.all(goods == updated)
 
-    assert updated.shape == (10, 4, 4)
-    for n, state in enumerate(states):
-        for i in range(4):
-            for j in range(4):
-                assert SBOX[state[i, j]] == updated[n, i, j]
 
 def test_ShiftRows():
-    s0 = np.arange(0, 16, dtype=np.uint8).reshape(4,4)
-    s1 = np.arange(16, 32, dtype=np.uint8).reshape(4,4)
-    s2 = np.arange(32, 48, dtype=np.uint8).reshape(4,4)
+    pts = np.random.randint(0, 256,
+                           size=(200,4,4), dtype=np.uint8)
+    goods = []
+    for pt in pts:
+        int_pt = int.from_bytes(pt.tobytes(), 'big')
+        goods.append(refAES.shift_rows(text2matrix(int_pt)))
+    goods = np.array(goods)
+    updated = ShiftRows(pts)
 
-    states = np.array([s0, s1, s2])
-
-    updated = ShiftRows(states)
-
-    good = np.array([
-                      [[0, 1, 2, 3],
-                       [5, 6, 7, 4],
-                       [10, 11, 8, 9],
-                       [15, 12, 13, 14]],
-
-                      [[16, 17, 18, 19],
-                       [21, 22, 23, 20],
-                       [26, 27, 24, 25],
-                       [31, 28, 29, 30]],
-
-                       [[32 ,33 ,34 ,35],
-                        [37 ,38 ,39 ,36],
-                        [42 ,43 ,40 ,41],
-                        [47 ,44 ,45 ,46]]], dtype=np.uint8)
-    assert np.all(updated == good)
+    assert np.all(goods == updated)
 
 
 def test_MixColumns():
-    s0 = np.arange(0, 16, dtype=np.uint8).reshape(4,4)
-    s1 = np.arange(16, 32, dtype=np.uint8).reshape(4,4)
-    s2 = np.arange(32, 48, dtype=np.uint8).reshape(4,4)
+    pts = np.random.randint(0, 256,
+                           size=(200,4,4), dtype=np.uint8)
 
-    states = np.array([s0, s1, s2])
-    updated = MixColumns(states)
+    goods = []
 
-    good = np.array(
-        [
-        [[ 8, 9, 10, 11],
-         [28, 29, 30, 31],
-         [ 0, 1, 2, 3],
-         [20, 21, 22, 23]],
+    for pt in pts:
+        int_pt = int.from_bytes(pt.tobytes(), 'big')
+        goods.append(refAES.mix_columns(text2matrix(int_pt)))
 
-        [[24, 25, 26, 27],
-         [12, 13, 14, 15],
-         [16, 17, 18, 19],
-         [4, 5, 6, 7]],
+    goods = np.array(goods)
+    updated = MixColumns(pts)
 
-        [[40, 41, 42, 43],
-         [60, 61, 62, 63],
-         [32, 33, 34, 35],
-         [52, 53, 54, 55]]], dtype=np.uint8)
-
-    assert np.all(updated == good)
+    assert np.all(goods == updated)
 
 
 def test_AddRoundKey():
@@ -93,8 +70,9 @@ def test_AddRoundKey():
     assert np.all(updated[1] == 8)
     assert np.all(updated[2] == 9)
 
+
 def test_ExpandKeys():
-    # TODO: TEST the entire key expansion
+    
     key = np.array([
         [0x2b, 0x7e, 0x15, 0x16],
         [0x28, 0xae, 0xd2, 0xa6],
@@ -102,16 +80,11 @@ def test_ExpandKeys():
         [0x09, 0xcf, 0x4f, 0x3c]
         ], dtype=np.uint8)
 
-    rk1 = np.array([
-        [0xa0, 0xfa, 0xfe, 0x17],
-        [0x88, 0x54, 0x2c, 0xb1],
-        [0x23, 0xa3, 0x39, 0x39],
-        [0x2a, 0x6c, 0x76, 0x05]
-        ], dtype=np.uint8)
-    keys = ExpandKeys(key)
+    ref_aes = refAES(int.from_bytes(key.tobytes(), 'big'))
 
-    assert np.all(keys[0] == key)
-    print(rk1)
-    print()
-    print(keys[1])
-    assert np.all(keys[1] == rk1)
+    good_rks = np.array(ref_aes.round_keys, 
+                       dtype=np.uint8).reshape(11, 4, 4)
+
+    rks = ExpandKeys(key)
+
+    assert np.all(good_rks == rks)
